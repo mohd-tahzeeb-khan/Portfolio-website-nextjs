a4cc833787b828f404450e741bc69e3839a551d6
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-03-07 17:05:37 +0530

    deploying on the servere

--- portfolio/src/prismicio.js
+++ portfolio/src/prismicio.js
@@ -1,9 +1,9 @@
 import * as prismic from "@prismicio/client";
 import config from "../slicemachine.config.json";
 
-export const repositoryName =process.env.NEXT_PUBLIC_API_URL;
-console.log("repository naame is",repositoryName);
-console.log("URL is",process.env);
+export const repositoryName ="https://tahzeeb-portfolio.cdn.prismic.io/api/v2";
+// console.log("repository naame is",repositoryName);
+// console.log("URL is",process.env);
 export const createClient = () => {
   return prismic.createClient(repositoryName, {
     accessToken: "" // Optional if using private content
