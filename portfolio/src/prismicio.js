0e8fdac550abe2000fd097501fff269398264981
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-03-07 16:48:15 +0530

    testing code

--- portfolio/src/prismicio.js
+++ portfolio/src/prismicio.js
@@ -2,7 +2,8 @@ import * as prismic from "@prismicio/client";
 import config from "../slicemachine.config.json";
 
 export const repositoryName =process.env.NEXT_PUBLIC_API_URL;
-
+console.log(repositoryName);
+console.log(process.env.NEXT_PUBLIC_API_URL);
 export const createClient = () => {
   return prismic.createClient(repositoryName, {
     accessToken: "" // Optional if using private content
