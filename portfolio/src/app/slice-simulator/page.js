1b2ab543e5bddeabf4e41f42189f9eab3c08146a
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-03-07 15:51:58 +0530

    deploying

--- portfolio/src/app/slice-simulator/page.js
+++ portfolio/src/app/slice-simulator/page.js
@@ -4,7 +4,7 @@ import {
 } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
+// import { components } from "../../slices";
 
 export default async function SliceSimulatorPage({ searchParams }) {
   const { state } = await searchParams;
